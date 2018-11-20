9c0dd8ca72476813de7e4a87a0127385e7c00dab
Daniel Clark <dajclark@googlemail.com>
2018-11-20 00:43:34 +0000

    Added model class

---
+++ app/src/main/java/com/example/recyclerexample2/TodoModel.java
@@ -0,0 +1,15 @@
+package com.example.recyclerexample2;
+
+class TodoModel {
+
+    static String[] Todos = {
+            "Todo One",
+            "Todo Two"
+    };
+
+    static String[] Todo = {
+            "Todo One\n\nWake up!",
+            "Todo Two\n\nGo to sleep!"
+    };
+
+}
